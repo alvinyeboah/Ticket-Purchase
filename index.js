7ad3a0e4f1016691b84ac481536b4e00cd792854
alvinyeboah <alvinyeboah5@gmail.com>
2024-01-13 21:23:18 +0000

    New

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { MongoClient } = require('mongodb');
+const uuid = require('uuid');
+// const uri = 'your_mongodb_connection_string';
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) => res.sendFile(__dirname +'/index.html'))
 app.use(express.static(__dirname + '/public'));
